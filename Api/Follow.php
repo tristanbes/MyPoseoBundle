9d9f80f66a1d1366e7bf350b747ec3aa59d013c2
Tristan Bessoussa <tristan.bessoussa@gmail.com>
2014-11-12 12:22:01 +0100

    Everyday i'm shuff, shuff, shuffeling

--- Api/Follow.php
+++ Api/Follow.php
@@ -9,6 +9,7 @@
 namespace Tristanbes\MyPoseoBundle\Api;
 
 use Guzzle\Http\Client;
+use Guzzle\Http\Message\Response;
 
 /**
  * SEO Follow given keywords
@@ -29,26 +30,63 @@ class Follow
     }
 
     /**
+     * Process the API response, provides error handling
+     *
+     * @param Response $response The guzzle response
+     *
+     * @throws \LogicException
+     *
+     * @return array
+     */
+    public function processResponse(Response $response)
+    {
+        $data = $response->json();
+
+        if ($data['state'] != 1) {
+            return new \LogicException($data['msg']);
+        }
+
+        return $data['data'];
+    }
+
+    /**
      * List account sites
      *
      * @return array
      */
     public function getSites()
     {
-        $request = $this->client->get('site',['debug' => true]);
+        $request  = $this->client->createRequest('GET', 'site');
         $response = $this->client->send($request);
 
-        return $response;
+        $data = $this->processResponse($response);
+
+        return $data;
+    }
+
+    /**
+     * Get a site
+     *
+     * @param int $id The site's id
+     *
+     * @return array
+     */
+    public function getSite($id)
+    {
+        $request  = $this->client->createRequest('GET', 'site/'.$id);
+        $response = $this->client->send($request);
+
+        $data = $this->processResponse($response);
+
+        return $data;
     }
 
     /**
      * Add one or several sites
      *
-     * @param array   $sites Array of urls you want to add
-     * @param string  $label Label of the site
-     * @param integer $group Group id
-     *
-     * @return array
+     * @param array  $sites Array of urls you want to add
+     * @param string $label Label of the site
+     * @param id     $group Group id
      */
     public function addSites(array $sites, $label, $group)
     {
@@ -68,17 +106,90 @@ class Follow
     }
 
     /**
-     * Retrurns a site
+     * Update a site's comment
      *
-     * @param integer $id Site id
+     * @param int    $id      The site's id
+     * @param string $comment The comment
      *
      * @return array
      */
-    public function getSite($id)
+    public function updateSite($id, $comment)
     {
-        $request = $this->client->get(sprintf('site/%d', $id));
+        $request = $this->client->createRequest('PUT', 'site/'.$id);
+        $query   = $request->getQuery();
+
+        $query->set('comment', $id);
+
         $response = $this->client->send($request);
 
-        return $response;
+        $data = $this->processResponse($response);
+
+        return $data;
+    }
+
+    /**
+     * Deletes a site
+     *
+     * @param int $id The site's id
+     *
+     * @return array
+     */
+    public function deleteSite($id)
+    {
+        $request  = $this->client->createRequest('DELETE', 'site/'.$id);
+        $response = $this->client->send($request);
+
+        $data = $this->processResponse($response);
+
+        return $data;
+    }
+
+    /**
+     * List keywords attached to a site
+     *
+     * @param int $id The site's id
+     *
+     * @return array
+     */
+    public function getKeyword($id)
+    {
+        $request = $this->client->createRequest('GET', 'keyword');
+        $query   = $request->getQuery();
+
+        $query->set('keyword', $id);
+
+        $response = $this->client->send($request);
+
+        $data = $this->processResponse($response);
+
+        return $data;
+    }
+
+    /**
+     * Add keywords
+     *
+     * @param int   $id           The site's id
+     * @param int   $keywordGroup ID keywords group
+     * @param int   $searchEngine ID search engine
+     * @param int   $idLocation   ID device. (0 => desktop, 1 => Mobile)
+     * @param array $idDevice     Keywords list
+     *
+     * @return array
+     */
+    public function addKeywords($id, $keywordGroup, $searchEngine = 2, $idLocation = 13, $idDevice = 0)
+    {
+        $request = $this->client->createRequest('POST', 'keyword');
+        $body    = $request->getBody();
+
+        $body->set('site', $id);
+        $body->set('keywordsGroup', $keywordGroup);
+        $body->set('keywordsSetups[0][id_engine]', $searchEngine);
+        $body->set('sites', $sites);
+
+        $response = $this->client->send($request);
+
+        $data = $this->processResponse($response);
+
+        return $data;
     }
 }
