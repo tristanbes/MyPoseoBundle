84098ffaf96720c53cddf4b686f13e49f300cc35
Tristan Bessoussa <tristan.bessoussa@gmail.com>
2016-08-23 16:34:47 +0200

    fix dynamic service cache compiler

--- CompilerPass/DynamicServiceCompilerPass.php
+++ CompilerPass/DynamicServiceCompilerPass.php
@@ -15,18 +15,18 @@ use Symfony\Component\DependencyInjection\Reference;
 /**
  * Class DynamicServiceCacheCompilerPass
  */
-class DynamicServiceCacheCompilerPass implements CompilerPassInterface
+class DynamicServiceCompilerPass implements CompilerPassInterface
 {
     public function process(ContainerBuilder $container)
     {
         if ($container->getParameter('my_poseo.api.cache_service_id') != null) {
-            $container->getDefinition('my_poseo.search')
-                ->addArgument(new Reference($container->getParameter('my_poseo.api.cache_service_id')))
-            ;
+            $container->getDefinition('my_poseo.search.client')
+                ->replaceArgument(3, new Reference($container->getParameter('my_poseo.api.cache_service_id'))
+            );
         }
 
         if ($container->getParameter('my_poseo.api.http_client') != null) {
-            $container->getDefinition('my_poseo.api.http_client')
+            $container->getDefinition('my_poseo.search.client')
                 ->replaceArgument(2, new Reference($container->getParameter('my_poseo.api.http_client'))
             );
         }
--- DependencyInjection/MyPoseoExtension.php
+++ DependencyInjection/MyPoseoExtension.php
@@ -11,7 +11,6 @@ namespace Tristanbes\MyPoseoBundle\DependencyInjection;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\Loader;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\Reference;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
 /**
